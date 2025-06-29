cbe91da892502ba6e3e7973f027b11aff968d53a
Anthony Trépanier <anthony.trepanier@hotmail.com>
2025-06-29 15:59:06 -0400

    adjust data

--- backend/index.ts
+++ backend/index.ts
@@ -9,12 +9,8 @@ const app = express();
 app.use(express.json());
 app.use(logger);
 
-app.use("/", (req, res) => {
-	res.send("hola");
-});
-
-app.use("/posts", postRoutes);
-app.use("/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(errorHandler);
 
